31f15823c5f6f52d81d519fb0b05437b0cdbfea3
waddlesplash <ajcsweb@gmail.com>
2012-12-17 16:05:22 -0500

    Initial import.

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+/* Homepage */
+$DEFAULT_PAGE = 'home';
+
+/* Header Links */
+$HEADER_SPACER = '|';
+/* If 'true', uses content/headeritem.template
+ * as the template for header items */
+$CUSTOM_HEADER = false;
+?>
